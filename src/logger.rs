4a0adf2b960738f55c0c1f3f6eb01c9c434cd589
ludndev <ludndev@gmail.com>
2025-02-08 16:38:15 +0100

    feat(): use formatted datetime instead of timestamp in logger

--- src/logger.rs
+++ src/logger.rs
@@ -24,14 +24,27 @@ pub struct Logger;
 
 impl Logger {
     pub fn log(level: LogLevel, message: &str) {
-        let at = Logger::current_timestamp();
+        let at = Logger::current_datetime();
         let level_str = LogLevel::from(level).as_str();
-        println!("{} [{}] {}", at, level_str, message);
+        println!("[{}] [{}] {}", at, level_str, message);
     }
 
     fn current_timestamp() -> u64 {
         let now = SystemTime::now().duration_since(UNIX_EPOCH);
         let timestamp = now.unwrap().as_secs();
         return timestamp;
+    }
+
+    fn current_datetime() -> String {
+        let seconds = Self::current_timestamp();
+
+        let days = seconds / 86400;
+        let hours = (seconds % 86400) / 3600;
+        let minutes = (seconds % 3600) / 60;
+        let seconds = seconds % 60;
+
+        let datetime = format!("{:04}-{:02}-{:02} {:02}:{:02}:{:02}", 1970 + days / 365, (days % 365) / 30 + 1, days % 30 + 1, hours, minutes, seconds);
+
+        return datetime;
     }
 }
