4aad4f4f05216d0cc7455061c9651f5a4c0274f6
ludndev <ludndev@gmail.com>
2025-03-26 20:24:36 +0100

    fix(): specify result type in stream_by_chunk match in response.rs

--- src/response.rs
+++ src/response.rs
@@ -323,7 +323,7 @@ impl Response {
             return Ok(());
         }
 
-        let _ = match self.stream_by_chunk(stream) {
+        let _ : Result<(), Error>  = match self.stream_by_chunk(stream) {
             Ok(_) => Ok(()),
             Err(error) => {
                 Logger::log(LogLevel::ERROR, format!("Error while streaming by chunk: {}", error).as_str());
